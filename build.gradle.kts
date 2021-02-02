df2dcc545c95bf490cfbb9fb1ba9396a5a84e670
Rubydesic <rubydesic@gmail.com>
2021-02-02 18:36:31 -0500

    Its no longer weird, for the most part. fabric/forge here we go!

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,8 +24,8 @@ dependencies {
     implementation("com.badlogicgames.gdx:gdx-bullet-platform:$gdxVersion:natives-desktop")
 
     // JOML for Math
-    implementation("org.joml:joml:1.10.0")
-    implementation("org.joml:joml-primitives:1.10.0")
+    api("org.joml:joml:1.10.0")
+    api("org.joml:joml-primitives:1.10.0")
 
     // Apache Commons Math for Linear Programming
     implementation("org.apache.commons", "commons-math3", "3.6.1")
@@ -34,8 +34,8 @@ dependencies {
     implementation("com.google.guava:guava:29.0-jre")
 
     // Jackson Binary Dataformat for Object Serialization
-    implementation("com.fasterxml.jackson.module", "jackson-module-kotlin", jacksonVersion)
-    implementation("com.fasterxml.jackson.dataformat", "jackson-dataformat-cbor", jacksonVersion)
+    api("com.fasterxml.jackson.module", "jackson-module-kotlin", jacksonVersion)
+    api("com.fasterxml.jackson.dataformat", "jackson-dataformat-cbor", jacksonVersion)
 
     // Trove for Fast Primitive Collections
     implementation("net.sf.trove4j", "trove4j", "3.0.3")
