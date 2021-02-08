5e3b7040620ba21159e4b28e71caf9842127f265
Rubydesic <rubydesic@gmail.com>
2021-02-08 18:42:35 -0500

    rework joml serialization module

--- src/main/kotlin/org/valkyrienskies/core/util/serialization/JOMLSerializationModule.kt
+++ src/main/kotlin/org/valkyrienskies/core/util/serialization/JOMLSerializationModule.kt
@@ -1,26 +1,30 @@
 package org.valkyrienskies.core.util.serialization
 
 import com.fasterxml.jackson.databind.module.SimpleModule
-import org.joml.Matrix3d
-import org.joml.Matrix3dc
-import org.joml.Matrix4d
-import org.joml.Matrix4dc
-import org.joml.Quaterniond
-import org.joml.Quaterniondc
-import org.joml.Vector3d
-import org.joml.Vector3dc
-import org.joml.Vector3i
-import org.joml.Vector3ic
-import org.joml.primitives.AABBd
-import org.joml.primitives.AABBdc
+import org.joml.*
+import org.joml.primitives.*
 
-class JOMLSerializationModule: SimpleModule() {
+class JOMLSerializationModule : SimpleModule() {
     init {
-        super.addAbstractTypeMapping(Vector3dc::class.java, Vector3d::class.java)
-        super.addAbstractTypeMapping(Quaterniondc::class.java, Quaterniond::class.java)
-        super.addAbstractTypeMapping(Matrix4dc::class.java, Matrix4d::class.java)
-        super.addAbstractTypeMapping(Matrix3dc::class.java, Matrix3d::class.java)
-        super.addAbstractTypeMapping(Vector3ic::class.java, Vector3i::class.java)
-        super.addAbstractTypeMapping(AABBdc::class.java, AABBd::class.java)
+        addAbstractTypeMapping<Vector3ic, Vector3i>()
+        addAbstractTypeMapping<Vector3fc, Vector3f>()
+        addAbstractTypeMapping<Vector3dc, Vector3d>()
+
+        addAbstractTypeMapping<Quaternionfc, Quaternionf>()
+        addAbstractTypeMapping<Quaterniondc, Quaterniond>()
+
+        addAbstractTypeMapping<Matrix4fc, Matrix4f>()
+        addAbstractTypeMapping<Matrix4dc, Matrix4d>()
+
+        addAbstractTypeMapping<Matrix3fc, Matrix3f>()
+        addAbstractTypeMapping<Matrix3dc, Matrix3d>()
+
+        addAbstractTypeMapping<AABBic, AABBi>()
+        addAbstractTypeMapping<AABBfc, AABBf>()
+        addAbstractTypeMapping<AABBdc, AABBd>()
+    }
+
+    private inline fun <reified A, reified B : A> addAbstractTypeMapping() {
+        super.addAbstractTypeMapping(A::class.java, B::class.java)
     }
 }
