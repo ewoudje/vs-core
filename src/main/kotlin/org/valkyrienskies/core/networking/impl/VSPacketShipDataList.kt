30c2d224567e0c950919a7c534279bd520802a17
Tri0de <best108@uw.edu>
2021-02-09 15:23:37 -0800

    Optimized VSPacketShipDataList.kt

--- src/main/kotlin/org/valkyrienskies/core/networking/impl/VSPacketShipDataList.kt
+++ src/main/kotlin/org/valkyrienskies/core/networking/impl/VSPacketShipDataList.kt
@@ -3,38 +3,26 @@ package org.valkyrienskies.core.networking.impl
 import com.fasterxml.jackson.module.kotlin.readValue
 import io.netty.buffer.ByteBuf
 import io.netty.buffer.ByteBufInputStream
+import io.netty.buffer.ByteBufOutputStream
 import org.valkyrienskies.core.game.ShipData
 import org.valkyrienskies.core.networking.IVSPacket
 import org.valkyrienskies.core.util.serialization.VSJacksonUtil
+import java.io.InputStream
+import java.io.OutputStream
 
-class VSPacketShipDataList private constructor(): IVSPacket {
+class VSPacketShipDataList private constructor() : IVSPacket {
 
-    private lateinit var shipDataList: List<ShipData>
-
-    fun getShipDataList() = shipDataList
+    lateinit var shipDataList: List<ShipData>
+        private set
 
     override fun write(byteBuf: ByteBuf) {
-        byteBuf.writeInt(shipDataList.size)
-        for (shipData in shipDataList) {
-            val bytes = VSJacksonUtil.defaultMapper.writeValueAsBytes(shipData)
-            byteBuf.writeInt(bytes.size)
-            byteBuf.writeBytes(bytes)
-        }
+        val byteBufAsOutputStream: OutputStream = ByteBufOutputStream(byteBuf)
+        VSJacksonUtil.defaultMapper.writeValue(byteBufAsOutputStream, shipDataList)
     }
 
     override fun read(byteBuf: ByteBuf) {
-        val newShipDataList: MutableList<ShipData> = ArrayList()
-        val listSize: Int = byteBuf.readInt()
-        for (i in 1 .. listSize) {
-            // Read how many bytes this ShipData is
-            val bytesSize = byteBuf.readInt()
-            // Create an input stream from byteBuf that is bytesSize bytes big
-            val bytesAsInputStream = ByteBufInputStream(byteBuf, bytesSize)
-            // Convert the input stream to a ShipData
-            val shipDataFromBytes = VSJacksonUtil.defaultMapper.readValue<ShipData>(bytesAsInputStream)
-            newShipDataList.add(shipDataFromBytes)
-        }
-        shipDataList = newShipDataList
+        val byteBufAsInputStream: InputStream = ByteBufInputStream(byteBuf)
+        shipDataList = VSJacksonUtil.defaultMapper.readValue(byteBufAsInputStream)
     }
 
     companion object {
