5093ea4949e323c1f634a08ead06906576944d45
Rubydesic <rubydesic@gmail.com>
2021-02-18 10:08:06 -0500

    remove all the iteration methods from ChunkClaim

--- src/main/kotlin/org/valkyrienskies/core/game/ChunkClaim.kt
+++ src/main/kotlin/org/valkyrienskies/core/game/ChunkClaim.kt
@@ -75,58 +75,6 @@ data class ChunkClaim(val xIndex: Int, val zIndex: Int) {
     fun contains(x: Int, z: Int) =
         (x in xStart .. xEnd) and (z in zStart .. zEnd)
 
-    inline fun <T> mapChunks(func: (x: Int, y: Int) -> T): MutableList<T> {
-        val list = ArrayList<T>(size)
-        for (chunkX in xStart..xEnd)
-            for (chunkY in zStart..zEnd)
-                list += func(chunkX, chunkY)
-        return list
-    }
-
-    inline fun iterateChunks(func: (x: Int, y: Int) -> Unit) {
-        for (chunkX in xStart..xEnd)
-            for (chunkY in zStart..zEnd)
-                func(chunkX, chunkY)
-    }
-
-    inline fun iterateBlocks(func: (x: Int, y: Int, z: Int) -> Unit) {
-        iterateChunks { chunkX, chunkZ ->
-            for (x in chunkX until chunkX + 16)
-                for (z in chunkZ until chunkZ + 16)
-                    for (y in 0 until 256)
-                        func(x, y, z)
-        }
-    }
-
-    fun chunkIterator(): Iterable<Pair<Int, Int>> {
-        return Iterable {
-            object : Iterator<Pair<Int, Int>> {
-                var curX = xStart
-                var curZ = zStart
-
-                override fun hasNext(): Boolean {
-                    return curZ <= zEnd
-                }
-
-                override fun next(): Pair<Int, Int> {
-                    if (!hasNext()) throw NoSuchElementException()
-
-                    val toReturn = Pair(curX, curZ)
-
-                    if (curX == xEnd) {
-                        curX = 0
-                        curZ++
-                    } else {
-                        curX++
-                    }
-
-                    return toReturn
-                }
-
-            }
-        }
-    }
-
     fun getCenterBlockCoordinates(destination: Vector3i): Vector3i {
         val minBlockX = xStart * 16
         val maxBlockX = (xEnd * 16) - 1
