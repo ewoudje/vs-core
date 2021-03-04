ede7d1942e0c9e9daacbd60d521d9e7d76b5784b
Tri0de <best108@uw.edu>
2021-03-04 00:17:43 -0700

    Added comments to SATConvexPolygonCollider

--- src/main/kotlin/org/valkyrienskies/core/collision/SATConvexPolygonCollider.kt
+++ src/main/kotlin/org/valkyrienskies/core/collision/SATConvexPolygonCollider.kt
@@ -16,24 +16,19 @@ object SATConvexPolygonCollider : ConvexPolygonCollider {
         temp3: CollisionRange
     ) {
         var minCollisionDepth = Double.MAX_VALUE
+        collisionResult.colliding = true // Initially assume that polygons are collided
 
         for (normal in normals) {
-            val firstCollisionRange: CollisionRangec = firstPolygon.getProjectionAlongAxis(normal, temp1)
-            val secondCollisionRange: CollisionRangec = secondPolygon.getProjectionAlongAxis(normal, temp2)
-
+            // Calculate the overlapping range of the projection of both polygons along the [normal] axis
             val overlappingCollisionRange: CollisionRange = temp3
-            val areRangesOverlapping = CollisionRangec.computeOverlap(
-                firstCollisionRange,
-                secondCollisionRange,
-                overlappingCollisionRange
-            )
+            val areRangesOverlapping = computeOverlapAlongNormal(firstPolygon, secondPolygon, normal, overlappingCollisionRange, temp1, temp2)
 
             if (!areRangesOverlapping) {
-                // Polygons are separated
+                // Polygons are separated along [normal], therefore they are NOT colliding
                 collisionResult.colliding = false
                 return
             } else {
-                // Polygons are colliding along this axis
+                // Polygons are colliding along this axis, doesn't guarantee if the polygons are colliding or not
                 val collisionDepth = overlappingCollisionRange.getRangeLength()
                 if (collisionDepth < minCollisionDepth) {
                     minCollisionDepth = collisionDepth
@@ -43,5 +38,27 @@ object SATConvexPolygonCollider : ConvexPolygonCollider {
                 }
             }
         }
+    }
+
+    @Suppress("MemberVisibilityCanBePrivate")
+    fun computeOverlapAlongNormal(
+        firstPolygon: ConvexPolygon,
+        secondPolygon: ConvexPolygon,
+        normal: Vector3dc,
+        overlappingCollisionRangeOutput: CollisionRange,
+        temp1: CollisionRange,
+        temp2: CollisionRange
+    ): Boolean {
+        // Check if the polygons are separated along the [normal] axis
+        val firstCollisionRange: CollisionRangec = firstPolygon.getProjectionAlongAxis(normal, temp1)
+        val secondCollisionRange: CollisionRangec = secondPolygon.getProjectionAlongAxis(normal, temp2)
+
+        // Calculate the overlapping range of the projection of both polygons along the [normal] axis
+        val overlappingCollisionRange: CollisionRange = overlappingCollisionRangeOutput
+        return CollisionRangec.computeOverlap(
+            firstCollisionRange,
+            secondCollisionRange,
+            overlappingCollisionRange
+        )
     }
 }
