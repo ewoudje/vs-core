02b7bb82a1ed7f0b56d62d34e9b4f965a89e9559
StewStrong <stew.strong.420@gmail.com>
2022-05-29 19:24:41 -0600

    Added CollisionRangec.computeCollisionTime()

--- src/main/kotlin/org/valkyrienskies/core/collision/CollisionRangec.kt
+++ src/main/kotlin/org/valkyrienskies/core/collision/CollisionRangec.kt
@@ -41,5 +41,36 @@ interface CollisionRangec {
                 pushLeftWithRespectToVelocity
             }
         }
+
+        /**
+         * @return The time when [collisionRange1] will collide with [collisionRange2] given [collisionRange1Velocity].
+         *         If they are initially overlapping then we return 0.0, if they will never overlap we return
+         *         [Double.POSITIVE_INFINITY]
+         */
+        fun computeCollisionTime(
+            collisionRange1: CollisionRangec,
+            collisionRange2: CollisionRangec,
+            collisionRange1Velocity: Double
+        ): Double {
+            val pushLeft = -collisionRange1.max + collisionRange2.min
+            val pushRight = -collisionRange1.min + collisionRange2.max
+
+            return if (pushRight <= 0 || pushLeft >= 0) {
+                // If vel is too small then assume that it will take infinite time to collide
+                if (abs(collisionRange1Velocity) < 1e-8) return Double.POSITIVE_INFINITY
+
+                // Compute time before pushRight is > 0 or pushLeft < 0
+                if (collisionRange1Velocity > 0) {
+                    if (pushLeft >= 0) pushLeft / collisionRange1Velocity
+                    else Double.POSITIVE_INFINITY
+                } else {
+                    if (pushRight <= 0) pushRight / collisionRange1Velocity
+                    else Double.POSITIVE_INFINITY
+                }
+            } else {
+                // Already overlapping, so time of impact is 0
+                0.0
+            }
+        }
     }
 }
---
+++ src/test/kotlin/org/valkyrienskies/core/collision/CollisionRangecTest.kt
@@ -0,0 +1,86 @@
+package org.valkyrienskies.core.collision
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class CollisionRangecTest {
+
+    @Test
+    fun testComputeCollisionTime1() {
+        val playerCollisionRange = CollisionRange(1.0, 2.0)
+        val shipCollisionRange = CollisionRange(3.0, 4.0)
+
+        run {
+            val playerVelocity = 1.0
+            assertEquals(1.0, CollisionRangec.computeCollisionTime(playerCollisionRange, shipCollisionRange, playerVelocity))
+        }
+
+        run {
+            val playerVelocity = 2.0
+            assertEquals(0.5, CollisionRangec.computeCollisionTime(playerCollisionRange, shipCollisionRange, playerVelocity))
+        }
+
+        run {
+            val playerVelocity = -1.0
+            assertEquals(Double.POSITIVE_INFINITY, CollisionRangec.computeCollisionTime(playerCollisionRange, shipCollisionRange, playerVelocity))
+        }
+
+        run {
+            val playerVelocity = 1e-30
+            assertEquals(Double.POSITIVE_INFINITY, CollisionRangec.computeCollisionTime(playerCollisionRange, shipCollisionRange, playerVelocity))
+        }
+    }
+
+    @Test
+    fun testComputeCollisionTime2() {
+        val playerCollisionRange = CollisionRange(1.0, 2.0)
+        val shipCollisionRange = CollisionRange(-1.0, 0.0)
+
+        run {
+            val playerVelocity = -1.0
+            assertEquals(1.0, CollisionRangec.computeCollisionTime(playerCollisionRange, shipCollisionRange, playerVelocity))
+        }
+
+        run {
+            val playerVelocity = -2.0
+            assertEquals(0.5, CollisionRangec.computeCollisionTime(playerCollisionRange, shipCollisionRange, playerVelocity))
+        }
+
+        run {
+            val playerVelocity = 1.0
+            assertEquals(Double.POSITIVE_INFINITY, CollisionRangec.computeCollisionTime(playerCollisionRange, shipCollisionRange, playerVelocity))
+        }
+
+        run {
+            val playerVelocity = -1e-30
+            assertEquals(Double.POSITIVE_INFINITY, CollisionRangec.computeCollisionTime(playerCollisionRange, shipCollisionRange, playerVelocity))
+        }
+    }
+
+    @Test
+    fun testComputeCollisionTime3() {
+        val playerCollisionRange = CollisionRange(1.0, 2.0)
+        val shipCollisionRange = CollisionRange(1.5, 2.5)
+
+        run {
+            val playerVelocity = 1.0
+            assertEquals(0.0, CollisionRangec.computeCollisionTime(playerCollisionRange, shipCollisionRange, playerVelocity))
+        }
+
+        run {
+            val playerVelocity = 2.0
+            assertEquals(0.0, CollisionRangec.computeCollisionTime(playerCollisionRange, shipCollisionRange, playerVelocity))
+        }
+
+        run {
+            val playerVelocity = -1.0
+            assertEquals(0.0, CollisionRangec.computeCollisionTime(playerCollisionRange, shipCollisionRange, playerVelocity))
+        }
+
+        run {
+            val playerVelocity = 1e-30
+            assertEquals(0.0, CollisionRangec.computeCollisionTime(playerCollisionRange, shipCollisionRange, playerVelocity))
+        }
+    }
+
+}
