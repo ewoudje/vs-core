c33af6a6aee5d701cb8ae69f8a16496287fa3f87
ewoudje <ewoud.dierickx@hotmail.be>
2022-06-03 17:30:53 +0200

    Use the BlockInfo for chunk updates

--- src/main/kotlin/org/valkyrienskies/core/game/VSBlockType.kt
+++ src/main/kotlin/org/valkyrienskies/core/game/VSBlockType.kt
@@ -1,5 +1,14 @@
 package org.valkyrienskies.core.game
 
+import org.valkyrienskies.physics_api.voxel_updates.KrunchVoxelStates
+
 enum class VSBlockType {
-    AIR, SOLID, WATER, LAVA
+    AIR, SOLID, WATER, LAVA;
+
+    fun toByte() = when (this) {
+        AIR -> KrunchVoxelStates.AIR_STATE
+        SOLID -> KrunchVoxelStates.SOLID_STATE
+        WATER -> KrunchVoxelStates.WATER_STATE
+        LAVA -> KrunchVoxelStates.LAVA_STATE
+    }
 }
