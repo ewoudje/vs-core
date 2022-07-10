6222e8a897b82a9ad4df324947fbe48f2c2fd99f
StewStrong <stew.strong.420@gmail.com>
2022-07-09 18:38:00 -0600

    Removed unused imports

--- src/main/kotlin/org/valkyrienskies/core/game/ships/ShipObjectServer.kt
+++ src/main/kotlin/org/valkyrienskies/core/game/ships/ShipObjectServer.kt
@@ -4,8 +4,6 @@ import com.google.common.collect.MutableClassToInstanceMap
 import org.joml.Matrix4dc
 import org.valkyrienskies.core.api.Ship
 import org.valkyrienskies.core.api.ShipForcesInducer
-import org.valkyrienskies.core.chunk_tracking.IShipChunkTracker
-import org.valkyrienskies.core.chunk_tracking.ShipChunkTracker
 import org.valkyrienskies.core.networking.delta.DeltaEncodedChannelServerTCP
 import org.valkyrienskies.core.util.serialization.VSJacksonUtil
 
