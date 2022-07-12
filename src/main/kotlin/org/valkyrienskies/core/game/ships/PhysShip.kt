d0102c99f8b77b83730e45ea131defeebf0b4018
ewoudje <7384674+ewoudje@users.noreply.github.com>
2022-07-12 13:42:48 +0200

    Add Pos and Rot to PhysShip

--- src/main/kotlin/org/valkyrienskies/core/game/ships/PhysShip.kt
+++ src/main/kotlin/org/valkyrienskies/core/game/ships/PhysShip.kt
@@ -1,5 +1,6 @@
 package org.valkyrienskies.core.game.ships
 
+import org.joml.Quaterniondc
 import org.joml.Vector3dc
 import org.valkyrienskies.core.api.ShipForcesInducer
 import org.valkyrienskies.physics_api.RigidBodyReference
@@ -9,6 +10,10 @@ data class PhysShip internal constructor(
     // Don't use these outside of vs-core, I beg of thee
     internal val rigidBodyReference: RigidBodyReference,
     internal val forceInducers: List<ShipForcesInducer>,
+
+    // TODO transformation matrix
+    val position: Vector3dc,
+    val rotation: Quaterniondc,
 
     val inertia: PhysInertia,
     val velocity: Vector3dc,
