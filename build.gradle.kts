53194caab22d6807e5540650676299c52759b663
StewStrong <stew.strong.420@gmail.com>
2022-07-13 01:41:43 -0600

    Updated Krunch

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,7 +52,7 @@ dependencies {
     val nettyVersion = "4.1.25.Final"
 
     // VS Physics
-    api("org.valkyrienskies:physics_api_krunch:1.0.0+e0d3a05d58")
+    api("org.valkyrienskies:physics_api_krunch:1.0.0+3df5535a71")
 
     // JOML for Math
     api("org.joml:joml:1.10.0")
