0f3f045a55d67680b1bb6354198249a8644f082a
StewStrong <stew.strong.420@gmail.com>
2022-07-31 03:08:03 -0600

    Added helper functions to SegmentUtils.kt

--- src/main/kotlin/org/valkyrienskies/core/pipelines/SegmentUtils.kt
+++ src/main/kotlin/org/valkyrienskies/core/pipelines/SegmentUtils.kt
@@ -1,5 +1,7 @@
 package org.valkyrienskies.core.pipelines
 
+import org.joml.Vector3d
+import org.joml.Vector3dc
 import org.valkyrienskies.physics_api.PoseVel
 import org.valkyrienskies.physics_api.Segment
 import org.valkyrienskies.physics_api.SegmentDisplacement
@@ -14,4 +16,71 @@ object SegmentUtils {
     fun createSegmentTrackerFromScaling(dimension: Int, scaling: Double): SegmentTracker {
         return SingleSegmentTracker(Segment(0, createSegmentDisplacementFromScaling(dimension, scaling)))
     }
+
+    fun transformPos(
+        poseVel: PoseVel, segmentDisplacement: SegmentDisplacement, pos: Vector3dc, dest: Vector3d
+    ): Vector3d {
+        poseVel.transformPosition(pos, dest)
+        segmentDisplacement.transformPosition(dest)
+        return dest
+    }
+
+    fun invTransformPos(
+        poseVel: PoseVel, segmentDisplacement: SegmentDisplacement, pos: Vector3dc, dest: Vector3d
+    ): Vector3d {
+        segmentDisplacement.invTransformPosition(pos, dest)
+        poseVel.invTransformPosition(dest)
+        return dest
+    }
+
+    fun transformDirectionWithScale(
+        poseVel: PoseVel, segmentDisplacement: SegmentDisplacement, dir: Vector3dc, dest: Vector3d
+    ): Vector3d {
+        poseVel.transformDirection(dir, dest)
+        segmentDisplacement.transformDirectionWithScale(dest)
+        return dest
+    }
+
+    fun invTransformDirectionWithScale(
+        poseVel: PoseVel, segmentDisplacement: SegmentDisplacement, dir: Vector3dc, dest: Vector3d
+    ): Vector3d {
+        segmentDisplacement.invTransformDirectionWithScale(dir, dest)
+        poseVel.invTransformDirection(dest)
+        return dest
+    }
+
+    fun transformDirectionWithoutScale(
+        poseVel: PoseVel, segmentDisplacement: SegmentDisplacement, dir: Vector3dc, dest: Vector3d
+    ): Vector3d {
+        poseVel.transformDirection(dir, dest)
+        segmentDisplacement.transformDirectionWithoutScale(dest)
+        return dest
+    }
+
+    fun invTransformDirectionWithoutScale(
+        poseVel: PoseVel, segmentDisplacement: SegmentDisplacement, dir: Vector3dc, dest: Vector3d
+    ): Vector3d {
+        segmentDisplacement.invTransformDirectionWithoutScale(dir, dest)
+        poseVel.invTransformDirection(dest)
+        return dest
+    }
+
+    fun getOmega(poseVel: PoseVel, segmentDisplacement: SegmentDisplacement, dest: Vector3d): Vector3d {
+        dest.set(poseVel.omega)
+        segmentDisplacement.transformDirectionWithoutScale(dest)
+        dest.add(segmentDisplacement.getPoseVelForInternalUseOnly().omega)
+        return dest
+    }
+
+    fun getVelocity(poseVel: PoseVel, segmentDisplacement: SegmentDisplacement, dest: Vector3d): Vector3d {
+        dest.set(poseVel.vel)
+        segmentDisplacement.transformDirectionWithScale(dest)
+        dest.add(segmentDisplacement.getPoseVelForInternalUseOnly().vel)
+        return dest
+    }
+
+    /**
+     * This will be implemented in the future for portals, but for now we just return 0 for all positions
+     */
+    fun getSegmentId(localPos: Vector3d): Int = 0
 }
