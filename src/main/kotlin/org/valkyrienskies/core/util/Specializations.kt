ae21c35d51928318a1531ba6c1fcf3b274199e6d
ewoudje <7384674+ewoudje@users.noreply.github.com>
2022-08-04 15:02:59 +0200

    Checkstyle

--- src/main/kotlin/org/valkyrienskies/core/util/Specializations.kt
+++ src/main/kotlin/org/valkyrienskies/core/util/Specializations.kt
@@ -3,3 +3,5 @@ package org.valkyrienskies.core.util
 interface DoubleTernaryConsumer {
     fun accept(x: Double, y: Double, z: Double)
 }
+
+class Spec // TODO remove, checkstyle crying about file name not equal to interface name
